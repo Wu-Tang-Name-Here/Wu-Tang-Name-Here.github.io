dc47155cc8c7bd7399f3eca1f08c2927dffae2b2
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-06-13 16:01:44 -0700

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -518,13 +518,23 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
-  window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  var items = document.getElementsByClassName('.mover');
+  var phaseTop = document.body.scrollTop;
+  var phaseArray = [];
+
+  var i;
+
+  for(i = 0; i < 5; i++) {
+  	phaseArray.push(Math.sin((phaseTop / 1250) + i));
   }
+
+  for(i = 0; i < items.length; i++) {
+  	var phase = phaseArray[i % 5];
+  	items[i].style.transform = 'translateX(' + 100 * phase + 'px)';
+  }
+
+  window.performance.mark("mark_start_frame");
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
