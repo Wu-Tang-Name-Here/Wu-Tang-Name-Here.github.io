566d922c05bb88929c3ab4ce95c5b03130f2a0aa
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-06-21 21:47:08 -0700

    Add files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,6 @@
 module.exports = function(grunt) {
 
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-inline');
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -25,9 +21,12 @@ module.exports = function(grunt) {
         inline: {
             dist: {
                 src: 'src/index.html',
+                dest: 'dist/index.html'
             }
         }
     });
-
-    grunt.registerTask('default', ['uglify, cssmin, inline']);
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-inline');
+    grunt.registerTask('default','inline');
 };
