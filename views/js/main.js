399fe9cc44669093a667de0278d0e4d0ce170195
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-06-22 08:57:19 -0700

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -545,16 +545,17 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem = "";
+  var movingPizzaOne = document.getElementById("movingPizzas1");
 
   for (var i = 0; i < 25; i++) {
-    var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "70px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector('#movingPizzas1").appendChild(elem);
+    movingPizzaOne.appendChild(elem);
   }
   updatePositions();
 });
